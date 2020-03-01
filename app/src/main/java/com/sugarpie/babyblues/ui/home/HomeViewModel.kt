45a20d995abf8307758a2bf8b943d950648a89a2
jestacio <jestacio@meetcircle.com>
2020-02-29 19:07:32 -0800

    1 adding updates for app launch to initial state

--- app/src/main/java/com/sugarpie/babyblues/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sugarpie/babyblues/ui/home/HomeViewModel.kt
@@ -3,11 +3,20 @@ package com.sugarpie.babyblues.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sugarpie.babyblues.data.Settings
+import java.util.*
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _settings = MutableLiveData<Settings>().apply {
+        val defaultTimestamp = GregorianCalendar(0, 0, 0)
+        defaultTimestamp.set(Calendar.YEAR, 1990)
+        defaultTimestamp.set(Calendar.MONTH, 10)
+        defaultTimestamp.set(Calendar.DAY_OF_MONTH, 23)
+        value = Settings(mapOf(
+            Settings.KEY_VERSION to "1",
+            Settings.KEY_REMINDERENABLED to false,
+            Settings.KEY_REMINDERTIMESTAMP to defaultTimestamp)) }
+
+    val settings: LiveData<Settings> = _settings
 }
